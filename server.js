8adbf290c3e0ba2ac65bdb2f52e7f44d04d4c73b
riserishitha <rishitha.n@kalvium.community>
2024-04-08 10:26:33 +0530

    adding ntts and connecting data

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require("express")
 var app = express()
+const {connectdb,model} = require("./mongodatabase")
+function getted(){
+    return model.db.readyState === 1
+}
 
 app.get("/ping",(req,res)=>{
-    return res.send("pong")
+    const checking = getted()
+    let con = checking? "connected" : "not connected"
+    return res.send(con)
 })
 
 app.listen(8080,()=>{
+    connectdb()
     console.log("This is Express.js file.")
 })
