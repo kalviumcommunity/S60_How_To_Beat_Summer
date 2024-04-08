03c5d4cc780fc3105acc5758852bd4e6a86a71dc
riserishitha <rishitha.n@kalvium.community>
2024-04-08 18:00:10 +0530

    dummy data

--- SUMMERBEAT(FE)/src/App.jsx
+++ SUMMERBEAT(FE)/src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
+import './App.css';
+import Ddata from './Dummydata';
 
 function App() {
   return (
     <>
-    <h1>
-      WELCOME
-    </h1>
-    <br />
-    <p>
-      This is my front end ...
-    </p>
+    <div>
+      <h2>Category: {Ddata.category}</h2>
+      <img src={Ddata.image} />
+      <h4>Health: {Ddata.health}</h4>
+      <h4>Beauty: {Ddata.beauty}</h4>
+      <h4>Dos: {Ddata.dos}</h4>
+      <h4>Donts: {Ddata.donts}</h4>
+    </div>
     </>
   )
 }
